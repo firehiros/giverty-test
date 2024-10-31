65a0b660d499029445c3e110b6ddb402dafad042
Gigagon <kieuquocchien@gmail.com>
2024-10-31 08:52:47 +0000

    update

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,12 +4,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 config();
 
 export const typeOrmConfig: TypeOrmModule = {
-  type: 'postgres',
-  host: process.env.POSTGRESQL_HOST,
-  port: process.env.POSTGRESQL_PORT,
-  username: process.env.POSTGRESQL_USERNAME,
-  password: process.env.POSTGRESQL_PASSWORD,
-  database: process.env.POSTGRESQL_DATABASE_NAME,
+  type: 'sqlite',
+  database: '.db',
   entities: [`${__dirname}/../../**/*.entity.{js,ts}`],
   synchronize: true,
   logging: true,
